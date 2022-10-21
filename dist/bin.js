0e9270e47b210f8d974966a3a151f10236d0da16
BuildBot <hi@devcomp.xyz>
2022-10-21 05:58:00 +0000

    chore(deploy): build rojo project

--- dist/bin.js
+++ dist/bin.js
@@ -25087,9 +25087,9 @@ const dir = core.getInput("working-directory");
 const execCallback = (stdout, stderr, err) => {
     if (err) {
         (0, logger_1.error)("buildProject    ", `${err.message}`);
-        throw new Error("Failed to build. Set $ACTIONS_BACKTRACE=1 to receive a backtrace.");
+        throw new Error("Failed to build. Set $ACTIONS_BACKTRACE=1 or enable actions debug mode to receive a backtrace.");
     }
-    if (process.env.ACTIONS_BACKTRACE == "1" && err) {
+    if ((process.env.ACTIONS_BACKTRACE == "1" || core.isDebug()) && err) {
         (0, logger_1.error)("buildProject    ", `Backtrace:- ${stdout} + Error:- ${stderr}`);
         throw new Error("Failed to build.");
     }
